d1417c0a5c84b922b0cf9d3d4695a92775cd6cec
lilei <445765368@qq.com>
2016-08-08 14:12:29 +0800

    s

--- AlertController/keyAnimation/ViewController.swift
+++ AlertController/keyAnimation/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
+    }   
 
 
     @IBOutlet var open: UIButton!
